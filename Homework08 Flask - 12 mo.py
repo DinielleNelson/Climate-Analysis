08dbe21d55dd3b94d08061979b7f2e15f382541b
Dinielle <Dinielle.Nelson@gmail.com>
2019-03-30 13:38:20 -0400

    completed required Flask apps

--- Homework08 Flask - 12 mo.py
+++ Homework08 Flask - 12 mo.py
@@ -36,7 +36,74 @@ def stations_2():
         stations = session.query(Station.station, Station.name).all()
         return jsonify(stations)
 
+@app.route("/api/v1.0/tobs")
+def temp_obs():
+        import datetime as dt
+        from datetime import datetime as dtdt
 
+        import sqlalchemy
+        from sqlalchemy.ext.automap import automap_base
+        from sqlalchemy.orm import Session
+        from sqlalchemy import create_engine, func
+
+        engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+        Base = automap_base()
+        Base.prepare(engine, reflect=True)
+        Measurement = Base.classes.measurement
+
+        session = Session(engine)
+        last_date = max(session.query(Measurement.date))
+        last_date2 = str(last_date[0])
+
+        dateObj = dtdt.strptime(last_date2,'%Y-%m-%d')
+        year_date = dateObj - dt.timedelta(days=365)
+
+        precip_l12 = session.query(Measurement.date, Measurement.tobs,Measurement.station).filter(Measurement.date >= year_date).all()
+        return jsonify(precip_l12)
+
+@app.route("/api/v1.0/<start>")
+def temp_start(start):
+        import datetime as dt
+        from datetime import datetime as dtdt
+
+        import sqlalchemy
+        from sqlalchemy.ext.automap import automap_base
+        from sqlalchemy.orm import Session
+        from sqlalchemy import create_engine, func
+
+        engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+        Base = automap_base()
+        Base.prepare(engine, reflect=True)
+        Measurement = Base.classes.measurement
+
+        session = Session(engine)
+        tobs_start =session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).all()
+        
+        return jsonify(tobs_start)
+        
+@app.route("/api/v1.0/<start>/<end>")
+def temp_startend(start,end):
+        import datetime as dt
+        from datetime import datetime as dtdt
+
+        import sqlalchemy
+        from sqlalchemy.ext.automap import automap_base
+        from sqlalchemy.orm import Session
+        from sqlalchemy import create_engine, func
+
+        engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+        Base = automap_base()
+        Base.prepare(engine, reflect=True)
+        Measurement = Base.classes.measurement
+
+        session = Session(engine)
+        tobs_startend =session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))\
+                .filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+        
+        return jsonify(tobs_startend)
 
 if __name__ == "__main__":
     app.run(debug=True)
